c200e1c72e84f757fc2e412d7e617ba1d385ec47
Marcelo Gobetti <marcelo.gobetti@cwi.com.br>
2018-08-01 01:17:50 -0300

    Added search button to action bar

--- app/src/main/java/me/gobetti/codechallenge/MainActivity.kt
+++ app/src/main/java/me/gobetti/codechallenge/MainActivity.kt
@@ -2,7 +2,11 @@ package me.gobetti.codechallenge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import me.gobetti.codechallenge.modules.list.ListFragment
+import android.app.SearchManager
+import android.content.Context
+import android.support.v7.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         loadFragment()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView?
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 
     private fun loadFragment() {
